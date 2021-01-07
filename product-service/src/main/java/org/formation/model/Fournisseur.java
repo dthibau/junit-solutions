44b3154426afc0f8687dae00202dfd8f536f0a5a
David THIBAU <david.thibau@gmail.com>
2021-01-07 16:06:19 +0100

    Fix Lombok

--- product-service/src/main/java/org/formation/model/Fournisseur.java
+++ product-service/src/main/java/org/formation/model/Fournisseur.java
@@ -5,13 +5,18 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Fournisseur {
+
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
--- product-service/src/main/java/org/formation/model/Produit.java
+++ product-service/src/main/java/org/formation/model/Produit.java
@@ -7,12 +7,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Produit {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
